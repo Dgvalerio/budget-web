e5d868847b0fd0fecea4a49e1317e1713f66c6b4
davigvalerio <al.davigvalerio@gmail.com>
2023-07-29 10:26:17 -0300

    chore(storybook): add styles configurations and install radix slot

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,24 @@
+
 import type { StorybookConfig } from "@storybook/nextjs";
+
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        name: "@storybook/addon-styling",
+        options: {
+          sass: {
+            implementation: require("sass"),
+          },
+        },
+      },
+    },
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
@@ -14,6 +28,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    const customConfig = { ...config };
+
+    if (customConfig.resolve && customConfig.resolve.alias) {
+      customConfig.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    }
+
+    return customConfig;
   },
 };
 export default config;
