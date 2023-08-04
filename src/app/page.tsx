9a6b61fb632e8ac89e0f78a46f1b7438f23128eb
davigvalerio <al.davigvalerio@gmail.com>
2023-08-04 18:01:27 -0300

    feat(home): add top bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 import { NextPage } from 'next';
 
+import { Heading } from '@/components/heading/heading';
+import { Text } from '@/components/text/text';
+
+import { ChevronDown, PiggyBank } from 'lucide-react';
+
 const Home: NextPage = () => (
-  <main>
-    <h1 className="text-5xl font-bold text-indigo-500">Hello World!</h1>
+  <main className="min-h-screen w-full">
+    <nav className="flex h-12 justify-between border-b-[1px] border-gray-850 text-gray-600">
+      <div className="flex items-center gap-2 px-4">
+        <PiggyBank className="h-8 w-8" />
+        <div className="flex flex-col">
+          <Text size="sm" className="leading-4">
+            Orçamento de
+          </Text>
+          <Heading size="md" className="leading-4">
+            Davi G. Valério
+          </Heading>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <div className="h-8 w-8 rounded-[50%] bg-gray-600"></div>
+        <div>
+          <ChevronDown className="h-8 w-8" />
+        </div>
+      </div>
+    </nav>
   </main>
 );
 
--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -8,21 +8,26 @@ export interface HeadingProps {
   size?: 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
 export const Heading: FC<HeadingProps> = ({
   size = 'md',
   asChild,
   children,
+  className,
 }) => {
   const Component = asChild ? Slot : 'h2';
 
   return (
     <Component
-      className={clsx({
-        'text-md font-medium tracking-[1.25px]': size === 'md',
-        'text-lg font-normal tracking-[0.25px]': size === 'lg',
-      })}
+      className={clsx(
+        {
+          'text-md font-medium tracking-[1.25px]': size === 'md',
+          'text-lg font-normal tracking-[0.25px]': size === 'lg',
+        },
+        className
+      )}
     >
       {children}
     </Component>
