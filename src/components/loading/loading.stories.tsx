e5ddeb0e5f0e0c3132e6bb66a68525e31235531e
davigvalerio <al.davigvalerio@gmail.com>
2023-08-04 16:16:29 -0300

    feat(loading): add loading component

---
+++ src/components/loading/loading.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Loading, LoadingProps } from '@/components/loading/loading';
+
+const meta = {
+  title: 'Input/Loading',
+  component: Loading,
+  parameters: { layout: 'centered' },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<LoadingProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
