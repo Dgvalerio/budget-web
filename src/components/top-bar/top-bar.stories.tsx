4ebe786109ad806f8892236c6856a3ed1200f324
davigvalerio <al.davigvalerio@gmail.com>
2023-08-05 20:09:59 -0300

    fix(top-bar): set next router on story

--- src/components/top-bar/top-bar.stories.tsx
+++ src/components/top-bar/top-bar.stories.tsx
@@ -1,3 +1,5 @@
+import { ReactElement } from 'react';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { TopBar, TopBarProps } from '@/components/top-bar/top-bar';
@@ -5,9 +7,21 @@ import { TopBar, TopBarProps } from '@/components/top-bar/top-bar';
 const meta = {
   title: 'Part/TopBar',
   component: TopBar,
-  parameters: { layout: 'centered' },
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
   tags: ['autodocs'],
   argTypes: {},
+  decorators: [
+    (Story): ReactElement => (
+      <div className="m-[-1rem] h-screen w-screen">
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<TopBarProps>;
 
 export default meta;
